1a7040edd69f194883851f08a8cd35acacac8ccf
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-08-03 22:14:24 +0200

    Extract Record code into separate file

--- src/ogn-web-gateway.ts
+++ src/ogn-web-gateway.ts
@@ -1,21 +1,5 @@
 import OGNClient from './ogn-client';
-
-const FPM_TO_MPS = 0.00507999983744;
-
-export interface Record {
-  from: string;
-  to: string | null;
-  time: number;
-  longitude: number | null;
-  latitude: number | null;
-  altitude: number | null;
-  verticalSpeed: number | null;
-  heading: number | null;
-  degreePerSecond: number | null;
-  speed: number | null;
-  symbol: string | null;
-  isReceiver: boolean;
-}
+import {convertRecord, encodeRecord} from './record';
 
 export default class OGNWebGateway {
   private readonly ognClient: OGNClient;
@@ -35,59 +19,11 @@ export default class OGNWebGateway {
   }
 
   onRecord(_record: any) {
-    let record = OGNWebGateway.convertRecord(_record);
+    let record = convertRecord(_record);
     if (!record) return;
 
-    let msg = OGNWebGateway.encodeRecord(record);
+    let msg = encodeRecord(record);
 
     console.log(msg);
-  }
-
-  static convertRecord(record: any): Record | null {
-    let from = record.from && record.from.call as (string | undefined);
-    if (!from) return null;
-
-    let data = record.data;
-    if (!data || data.constructor.name !== 'Position' || !data.timestamp) return null;
-
-    let degreePerSecond: number | null = null;
-    let verticalSpeed: number | null = null;
-
-    let match = data.comment && data.comment.match(/(?:([-+]\d+)fpm).*(?:([-+][0-9]*\.?[0-9]+)rot)/);
-    if (match) {
-      verticalSpeed = parseFloat(match[1]) * FPM_TO_MPS;
-      degreePerSecond = parseFloat(match[2]);
-    }
-
-    let to = (record.via && record.via.length && record.via.pop().call) || null;
-    let time = Date.parse(data.timestamp);
-    let longitude = (data.longitude !== undefined) ? data.longitude : null;
-    let latitude = (data.latitude !== undefined) ? data.latitude : null;
-    let altitude = data.altitude ? data.altitude : null;
-    let heading = data.extension && data.extension.courseDeg !== undefined ? data.extension.courseDeg : null;
-    let speed = data.extension && data.extension.speedMPerS !== undefined ? data.extension.speedMPerS : null;
-    let symbol = data.symbol || null;
-    let isReceiver = symbol === 'I&';
-
-    return {
-      from, to, time, longitude, latitude, altitude, verticalSpeed,
-      heading, degreePerSecond, speed, symbol, isReceiver,
-    };
-  }
-
-  static encodeRecord(record: Record): string {
-    return [
-      record.from,
-      record.to,
-      record.time,
-      (record.longitude !== null) ? Math.round(record.longitude * 100000) / 100000 : null,
-      (record.latitude !== null) ? Math.round(record.latitude * 100000) / 100000 : null,
-      (record.altitude !== null) ? Math.round(record.altitude) : null,
-      (record.verticalSpeed !== null) ? Math.round(record.verticalSpeed * 100) / 100 : null,
-      record.heading,
-      record.degreePerSecond,
-      (record.speed !== null) ? Math.round(record.speed * 100) / 100 : null,
-      record.symbol,
-    ].map(it => (it === null) ? '' : it).join('|');
   }
 }
